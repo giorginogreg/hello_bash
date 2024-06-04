7f066fae881b32f49dc3e4efbb7084a93060e564
Gregorio Giorgino <giorgino.greg@gmail.com>
2024-06-04 12:52:06 +0200

    Added echo params explanation

--- name_as_first_arg.sh
+++ name_as_first_arg.sh
@@ -8,4 +8,4 @@
 name=$1
 echo -n "Hello, " #If one wants to exclude the trailing newline character, the -n option can be passed
 echo -n ${name:=World}
-echo -e "!\n"
+echo -e "!\n" # Special characters, like tabs (\t), newline characters (\n), carriage return characters (\r) (i.e. for translating newlines from DOS format to Unix format), and any other ASCII character given by its code value) can be passed to the echo command with the -e option. For example, to output three strings, each on different lines, in a single echo command, use: echo -e "first line\nsecond line\nthird line".

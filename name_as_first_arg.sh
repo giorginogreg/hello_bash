f34448bd9f86b83724102508d83b7c4d52ce542a
Gregorio Giorgino <giorgino.greg@gmail.com>
2024-06-04 11:48:15 +0200

    Added more scripts: echo from a file, echoing first  arg

---
+++ name_as_first_arg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#In a bash script or function, $1 denotes the initial argument passed,
+# $2 denotes the second argument passed, and so forth.
+
+#This script takes a name as a command-line argument and prints a personalized greeting.
+
+echo "Hello, $1!"
